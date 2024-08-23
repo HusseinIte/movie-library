27b818dfed28ad36d1ce6259822bc759d0ff1616
HusseinIte <hussein.hamdan.ite@gmail.com>
2024-08-23 20:30:36 +0300

    create classes and database

---
+++ app/Service/AuthService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Service;
+
+
+use Illuminate\Http\Request;
+
+class AuthService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct(Request $request)
+    {
+        //
+    }
+}
+
